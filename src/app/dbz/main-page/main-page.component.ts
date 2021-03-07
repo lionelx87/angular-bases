3700f2b9e67b13d8b5dd619a3456709a8ee171c2
Lionel Acosta <lionelx87@gmail.com>
2021-03-07 15:54:57 -0300

    Uso de ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personaje {
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
+  personaje: Personaje;
+  personajes: Personaje[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.personaje = { name: '', power: 0 };
+    this.personajes = [
+      { name: 'Goku', power: 15000 },
+      { name: 'Vegeta', power: 14000 },
+    ]
+  }
+
+  add() {
+    this.personajes.push(this.personaje);
+    this.personaje = { name: '', power: 0 };
   }
 
 }
